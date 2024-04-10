11f1f868a1c1d5d08f7e04664618247e056444a6
Asher-Ul-Haque <contactasher.u.haque@gmail.com>
2024-04-10 20:05:39 +0530

    Floor in Binary Tree

--- Data_Structures/Trees/BinaryTree.java
+++ Data_Structures/Trees/BinaryTree.java
@@ -71,6 +71,39 @@ public class BinarySearchTree<Key extends Comparable<Key>>, <Value>
 		return null;
 	}
 	
+	public Key floor(Key key)
+	{
+		Node x = floor(root, key);
+		if (x == null) return null;
+		return x.key;
+	}
+	
+	private Node floor(Node x, Key key)
+	{
+		if (x == null)
+		{
+			return null;
+		}
+		int cmp = key.compareTo(x.key);
+		if (cmp == 0)
+		{
+			return x;
+		}
+		if (cmp < 0)
+		{
+			return floor(x.left, key);
+		}
+		Node temp = floor(x.right, key);
+		if (t != null)
+		{
+			return t;
+		}
+		else
+		{
+			return x;
+		}
+	}
+	
 	public void delete(Key key)
 	{
 	}
